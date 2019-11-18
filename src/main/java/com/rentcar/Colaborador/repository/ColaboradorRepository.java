c35902040cbb3f2444c33c849a54c589d3defb23
Matias Barria <matiasmaverick@gmail.com>
2019-11-18 17:09:08 -0300

    implementacion de estereotipo y extends

--- src/main/java/com/rentcar/Colaborador/repository/ColaboradorRepository.java
+++ src/main/java/com/rentcar/Colaborador/repository/ColaboradorRepository.java
@@ -2,8 +2,12 @@ package com.rentcar.Colaborador.repository;
 
 import com.rentcar.Colaborador.entity.ColaboradorEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
 
+@Repository
 public interface ColaboradorRepository extends MongoRepository<ColaboradorEntity, Serializable> {
+
+
 }
