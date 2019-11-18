103d2f72ff4f12c12bf4333ca691ad14c72a9340
josephcardenas <joseph.card@hotmail.com>
2019-11-18 17:05:33 -0300

    colaboradorController completado y actualizado

--- src/main/java/com/rentcar/Colaborador/controller/ColaboradorController.java
+++ src/main/java/com/rentcar/Colaborador/controller/ColaboradorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping(value =  "/cliente")
+@RequestMapping(value =  "/colaborador")
 @CrossOrigin(value={})
 
 
